4437468a60f141958d6bb26c87e86cf57dd26528
sword-smith <thor@neptune.cash>
2023-01-14 19:12:40 +0100

    Expand on tree transformation `graft`

--- src/graft.rs
+++ src/graft.rs
@@ -1,51 +1,64 @@
 use crate::ast::{self};
 use itertools::Itertools;
 
+fn rust_type_path_to_data_type(rust_type_path: &syn::TypePath) -> ast::DataType {
+    assert_eq!(
+        1,
+        rust_type_path.path.segments.len(),
+        "Length other than one not supported"
+    );
+    rust_type_path.path.segments[0].ident.to_string().into()
+}
+
+fn rust_type_to_data_type(x: &syn::Type) -> ast::DataType {
+    match x {
+        syn::Type::Path(data_type) => rust_type_path_to_data_type(&data_type),
+        other_type => panic!("{other_type:#?}"),
+    }
+}
+
 fn graft_fn_arg(rust_fn_arg: &syn::FnArg) -> ast::FnArg {
     match rust_fn_arg {
         syn::FnArg::Typed(pat_type) => {
             let name = match pat_type.pat.as_ref() {
                 syn::Pat::Ident(ident) => ident.ident.to_string(),
-                _ => panic!("unsupported"),
+                other => panic!("unsupported: {other:?}"),
             };
-            let data_type = match pat_type.ty.as_ref() {
-                syn::Type::Path(type_path) => {
-                    assert_eq!(
-                        1,
-                        type_path.path.segments.len(),
-                        "Length other than one not supported"
-                    );
-                    type_path.path.segments[0].ident.to_string()
-                }
-                _ => panic!("unsupported"),
+            let data_type: ast::DataType = match pat_type.ty.as_ref() {
+                syn::Type::Path(type_path) => rust_type_path_to_data_type(type_path),
+                other => panic!("unsupported: {other:?}"),
             };
 
             ast::FnArg {
                 name,
-                data_type: data_type.into(),
+                data_type: data_type,
             }
         }
-        syn::FnArg::Receiver(_) => panic!("Unsupported"),
+        other => panic!("unsupported: {other:?}"),
     }
 }
 
 fn graft_return_type(rust_return_type: &syn::ReturnType) -> Vec<ast::DataType> {
-    let ret_type_name = match rust_return_type {
+    let ret_type = match rust_return_type {
         syn::ReturnType::Type(_, path) => match path.as_ref() {
-            syn::Type::Path(type_path) => {
-                assert_eq!(
-                    1,
-                    type_path.path.segments.len(),
-                    "Length other than one not supported"
-                );
-                type_path.path.segments[0].ident.to_string()
+            syn::Type::Path(type_path) => vec![rust_type_path_to_data_type(type_path)],
+            syn::Type::Tuple(tuple_type) => {
+                let tuple_type = tuple_type;
+                println!("{tuple_type:#?}");
+                let elements = tuple_type
+                    .elems
+                    .iter()
+                    .map(|x| rust_type_to_data_type(x))
+                    .collect_vec();
+
+                elements
             }
-            _ => panic!("unsupported"),
+            _ => panic!("unsupported: {path:?}"),
         },
-        syn::ReturnType::Default => panic!("unsupported"),
+        other => panic!("unsupported: {other:?}"),
     };
 
-    vec![ret_type_name.into()]
+    ret_type
 }
 
 pub fn graft(input: &syn::ItemFn) -> ast::Fn {
@@ -70,16 +83,35 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_graft() {
+    fn u32_add() {
         let tokens: syn::Item = parse_quote! {
             fn add_u32(lhs: u32, rhs: u32) -> u32 {
-                return lhs + rhs;
+                let c = lhs + rhs;
+                return c;
             }
         };
         match &tokens {
             syn::Item::Fn(item_fn) => {
+                println!("{item_fn:#?}");
                 let ret = graft(item_fn);
-                println!("{:#?}", ret);
+                println!("{ret:#?}");
+            }
+            _ => panic!("unsupported"),
+        }
+    }
+
+    #[test]
+    fn u32_swap() {
+        let tokens: syn::Item = parse_quote! {
+            fn swap_u32(lhs: u32, rhs: u32) -> (u32, u32) {
+                return (rhs, lhs);
+            }
+        };
+        match &tokens {
+            syn::Item::Fn(item_fn) => {
+                println!("{item_fn:#?}");
+                let ret = graft(item_fn);
+                println!("{ret:#?}");
             }
             _ => panic!("unsupported"),
         }
