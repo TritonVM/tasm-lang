bfd8e3eeec774b3f77b3a6031638b703148b3e8b
Einar Rasmussen <einar@neptune.cash>
2023-03-30 00:10:06 +0200

    keep cast test

--- src/tests/programs/arithmetic/bfe.rs
+++ src/tests/programs/arithmetic/bfe.rs
@@ -68,6 +68,15 @@ mod run_tests {
     use crate::tests::shared_test::*;
 
     #[test]
+    fn instantiate_bfe_test() {
+        compare_prop_with_stack(
+            &instantiate_bfe_with_literal(),
+            vec![],
+            vec![bfe_lit(400u64.into()), bfe_lit(500u64.into())],
+        );
+    }
+
+    #[test]
     fn add_bfe_test() {
         compare_prop_with_stack(
             &add_bfe_rast(),
@@ -85,6 +94,20 @@ mod run_tests {
                 bfe_lit(10_000_000_000u64.into()),
             ],
             vec![bfe_lit(7766279652927078395u64.into())],
+        );
+    }
+
+    #[test]
+    fn cast_from_bool_test() {
+        compare_prop_with_stack(
+            &cast_from_bool_rast(),
+            vec![bool_lit(false)],
+            vec![bfe_lit(0u64.into())],
+        );
+        compare_prop_with_stack(
+            &cast_from_bool_rast(),
+            vec![bool_lit(true)],
+            vec![bfe_lit(1u64.into())],
         );
     }
 }
