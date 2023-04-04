7c43e28884852b678c23fb685ae528148fa8f392
sword-smith <thv@neptune.cash>
2023-04-04 16:41:28 +0200

    Add test with long, memory-spilling BFE expression

--- src/tests/programs/arithmetic/bfe.rs
+++ src/tests/programs/arithmetic/bfe.rs
@@ -49,6 +49,73 @@ fn cast_from_bool_rast() -> syn::ItemFn {
     })
 }
 
+#[allow(dead_code)]
+fn long_bfe_expr_rast() -> syn::ItemFn {
+    item_fn(parse_quote! {
+        fn long_bfe_expr(
+            input_0: BFieldElement,
+            input_1: BFieldElement,
+            input_2: BFieldElement,
+            input_3: BFieldElement,
+            input_4: BFieldElement,
+            input_5: BFieldElement,
+        ) -> (BFieldElement, BFieldElement, BFieldElement) {
+            let val0: u64 = 0;
+            let val1: BFieldElement = BFieldElement::new(0u64);
+            let val2: BFieldElement = BFieldElement::new(0u64);
+            let mut res0: BFieldElement = input_0 * BFieldElement::new(2u64)
+                + (input_1 * BFieldElement::new(2u64)
+                    + (input_2 * BFieldElement::new(2u64)
+                        + (input_3 * BFieldElement::new(2u64)
+                            + (input_4 * BFieldElement::new(2u64) + input_5 * BFieldElement::new(2u64)))))
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + BFieldElement::new(2u64) * input_1
+                + (true || false) as BFieldElement;
+            let res1: BFieldElement = res0;
+            let val3: BFieldElement = BFieldElement::new(0u64);
+            let val4: BFieldElement = BFieldElement::new(0u64);
+            let val5: BFieldElement = BFieldElement::new(0u64);
+            let val6: BFieldElement = BFieldElement::new(0u64);
+            let val7: BFieldElement = BFieldElement::new(0u64);
+            let val8: BFieldElement = BFieldElement::new(0u64);
+            let val9: BFieldElement = BFieldElement::new(0u64);
+            let val10: BFieldElement = BFieldElement::new(0u64);
+            let val11: BFieldElement = BFieldElement::new(0u64);
+            let val12: BFieldElement = BFieldElement::new(0u64);
+            let val13: BFieldElement = BFieldElement::new(0u64);
+            let val14: BFieldElement = BFieldElement::new(0u64);
+            let val15: BFieldElement = BFieldElement::new(0u64);
+            let val16: BFieldElement = BFieldElement::new(0u64);
+            let val17: BFieldElement = BFieldElement::new(0u64);
+            let val18: BFieldElement = BFieldElement::new(0u64);
+            res0 = res0 + input_0 + input_1 + input_2 + input_3 + input_4 + input_5 + res0;
+            res0 = res0 * BFieldElement::new(2u64)
+                + input_0 * BFieldElement::new(2u64)
+                + ((input_1 * BFieldElement::new(2u64) + input_2 * BFieldElement::new(2u64)))
+                + (input_3 * BFieldElement::new(2u64) + input_4 * BFieldElement::new(2u64) + input_5 * BFieldElement::new(2u64))
+                    * BFieldElement::new(2u64)
+                + input_0
+                + input_1
+                + input_2;
+            let res2: BFieldElement = res0 * res0 + 2;
+
+            return (res0, res1, res2);
+        }
+    })
+}
+
 #[cfg(test)]
 mod compile_and_typecheck_tests {
     use super::*;
@@ -130,6 +197,26 @@ mod run_tests {
             &cast_from_bool_rast(),
             vec![bool_lit(true)],
             vec![bfe_lit(1u64.into())],
+        );
+    }
+
+    #[test]
+    fn long_bfe_expr_test() {
+        compare_prop_with_stack(
+            &long_bfe_expr_rast(),
+            vec![
+                bfe_lit(3u64.into()),
+                bfe_lit(4u64.into()),
+                bfe_lit(5u64.into()),
+                bfe_lit(6u64.into()),
+                bfe_lit(7u64.into()),
+                bfe_lit(8u64.into()),
+            ],
+            vec![
+                bfe_lit(902u64.into()),
+                bfe_lit(179u64.into()),
+                bfe_lit(813606u64.into()),
+            ],
         );
     }
 }
