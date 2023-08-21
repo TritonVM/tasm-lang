c84ab6cc9b23ba3a65dda33579919dc3d9c7a5df
Thorkil Vaerge <thor@neptune.cash>
2023-08-21 18:15:15 +0200

    More OZK tests to own directory structure

--- src/tests_and_benchmarks.rs
+++ src/tests_and_benchmarks.rs
@@ -1,4 +1,5 @@
 mod benchmarks;
 pub mod ozk_programs;
+mod ozk_tests;
 mod programs;
 mod test_helpers;
--- src/tests_and_benchmarks/programs/arithmetic/bfe.rs
+++ src/tests_and_benchmarks/programs/arithmetic/bfe.rs
@@ -324,31 +324,3 @@ mod run_tests {
         );
     }
 }
-
-mod ozk_tests {
-    use crate::tests_and_benchmarks::{
-        ozk_programs,
-        test_helpers::{shared_test::*, *},
-    };
-    use triton_vm::{BFieldElement, NonDeterminism};
-
-    #[test]
-    fn add_bfe_ozk_test() {
-        // Test function on host machine
-        let input = vec![];
-        let non_determinism = NonDeterminism::new(vec![]);
-        let expected_output = vec![BFieldElement::new(29)];
-        let native_output = io_native::wrap_main_with_io(&ozk_programs::bfe_add::main)(
-            input.clone(),
-            non_determinism.clone(),
-        );
-        assert_eq!(native_output, expected_output);
-
-        // Test function in Triton VM
-        let parsed = ozk_parsing::parse_main("bfe_add");
-        let expected_stack_diff = 0;
-        let stack_start = vec![];
-        let vm_output = execute_with_stack(&parsed, stack_start, expected_stack_diff).unwrap();
-        assert_eq!(expected_output, vm_output.output);
-    }
-}
