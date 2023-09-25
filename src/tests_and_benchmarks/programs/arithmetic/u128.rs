5bdcd76f6a5783bc063d8377529d956aee010f30
Thorkil Vaerge <thor@neptune.cash>
2023-09-25 17:55:04 +0200

    Add test for u128 add

--- src/tests_and_benchmarks/programs/arithmetic/u128.rs
+++ src/tests_and_benchmarks/programs/arithmetic/u128.rs
@@ -1,7 +1,9 @@
 #[cfg(test)]
 mod run_tests {
+    use itertools::Itertools;
     use rand::{thread_rng, Rng};
     use syn::parse_quote;
+    use twenty_first::shared_math::other::random_elements;
 
     use crate::{graft::item_fn, tests_and_benchmarks::test_helpers::shared_test::*};
 
@@ -22,6 +24,49 @@ mod run_tests {
                     let a: u128 = u128::MAX;
                     let b: u32 = u128::BITS;
                     return (a, b);
+                }
+            })
+        }
+    }
+
+    #[test]
+    fn u128_add_test() {
+        let values_lhs: Vec<u128> = random_elements::<u128>(10)
+            .into_iter()
+            .map(|x| x / 2)
+            .collect_vec();
+        let values_rhs: Vec<u128> = random_elements::<u128>(10)
+            .into_iter()
+            .map(|x| x / 2)
+            .collect_vec();
+        let mut test_cases = values_lhs
+            .into_iter()
+            .zip(values_rhs)
+            .map(|(lhs, rhs)| {
+                InputOutputTestCase::new(
+                    vec![u128_lit(lhs), u128_lit(rhs)],
+                    vec![u128_lit(lhs + rhs)],
+                )
+            })
+            .collect_vec();
+        test_cases.push(InputOutputTestCase::new(
+            vec![u128_lit(0), u128_lit(0)],
+            vec![u128_lit(0)],
+        ));
+        test_cases.push(InputOutputTestCase::new(
+            vec![u128_lit(u128::MAX), u128_lit(0)],
+            vec![u128_lit(u128::MAX)],
+        ));
+        test_cases.push(InputOutputTestCase::new(
+            vec![u128_lit(u128::MAX - 1), u128_lit(1)],
+            vec![u128_lit(u128::MAX)],
+        ));
+        multiple_compare_prop_with_stack_safe_lists(&u128_add_rast(), test_cases);
+
+        fn u128_add_rast() -> syn::ItemFn {
+            item_fn(parse_quote! {
+                fn u128_add_test_fn(lhs: u128, rhs: u128) -> u128 {
+                    return lhs + rhs;
                 }
             })
         }
