a17f4c07a32bcbe9b9801aecca7471649201e3c6
Thorkil Vaerge <thor@neptune.cash>
2023-11-20 16:43:20 +0100

    WIP: work on enum-type moving to stack

--- src/tasm_code_generator/data_type/enum_type.rs
+++ src/tasm_code_generator/data_type/enum_type.rs
@@ -2,6 +2,7 @@ use itertools::Itertools;
 use triton_vm::instruction::LabelledInstruction;
 use triton_vm::triton_asm;
 use triton_vm::triton_instr;
+use triton_vm::BFieldElement;
 
 use crate::ast_types;
 use crate::ast_types::EnumType;
@@ -13,137 +14,277 @@ impl EnumType {
     /// BEFORE: _ *discriminant
     /// AFTER:  _ [data] [padding] discriminant
     pub(crate) fn load_from_memory(&self, state: &mut CompilerState) -> Vec<LabelledInstruction> {
-        // Strategy:
-        // get_variant_data_fields_in_memory
-        // _ *discriminant [(*data_field, stack_size)] field_count
-        let (subroutine, subroutines) = self.get_variant_data_pointers_and_field_sizes_at_runtime();
-        let get_variant_data_size_label = subroutine.get_label();
-        for sr in [vec![subroutine], subroutines].concat() {
+        let (pointer_pointer, get_variant_data_pointers, subroutines) =
+            self.get_variant_data_pointers(state);
+        let get_variant_data_pointers_label = get_variant_data_pointers.get_label();
+        for sr in [vec![get_variant_data_pointers], subroutines].concat() {
             state.add_library_function(sr);
         }
 
         let mut code = triton_asm!(
             dup 0
-            // _ *discriminant *discriminant
+            // _ *discriminant
 
-            call {get_variant_data_size_label}
-            // _ *discriminant [(*data_field, stack_size, is_reference_type)] field_count
+            call {get_variant_data_pointers_label}
+            // _
         );
 
-        // For now we assume that max number of fields is 1.
-        // If that were to change the field meta data needs to be stored in
-        // memory!
-        // We *should* be able to statically allocate the correct amount of data for that,
-        // I think.
+        todo!()
 
-        let [load_field_data_from_memory, load_field_reference_type_branch, load_field_value_branch] =
-            read_data_size_elements_to_stack_loop();
-        let load_field_data_from_memory_label = load_field_data_from_memory.get_label();
-        state.add_library_function(load_field_data_from_memory);
-        state.add_library_function(load_field_reference_type_branch);
-        state.add_library_function(load_field_value_branch);
+        // We have all pointers now. Now we should loop over them. But how do we
+        // know what type each field has? We don't! We probably need to
+        // *keep* the discriminant around and then write the code to copy a field
+        // from memory to stack based on the knowledge of the types.
 
-        let load_variant_data_from_memory_label = "load_enum_variant_data_to_stack_loop";
-        let read_data_size_elements_to_stack_loop: SubRoutine = triton_asm!(
-            // Invariant: _ *discriminant [(*data_field, stack_size is_reference_type)] remaining_fields
-        {load_variant_data_from_memory_label}:
-            // Check loop-condition, remaining_fields == 0
-            dup 0
-            push 0
-            eq
-            skiz
-                return
+        // let [load_field_data_from_memory, load_field_reference_type_branch, load_field_value_branch] =
+        //     read_data_size_elements_to_stack_loop();
+        // let load_field_data_from_memory_label = load_field_data_from_memory.get_label();
+        // state.add_library_function(load_field_data_from_memory);
+        // state.add_library_function(load_field_reference_type_branch);
+        // state.add_library_function(load_field_value_branch);
 
-            // _ *discriminant [other_fields] *next_field next_size is_reference_type remaining_fields
+        // let load_variant_data_from_memory_label = "load_enum_variant_data_to_stack_loop";
+        // let read_data_size_elements_to_stack_loop: SubRoutine = triton_asm!(
+        //     // Invariant: _ *discriminant [(*data_field, stack_size is_reference_type)] remaining_fields
+        // {load_variant_data_from_memory_label}:
+        //     // Check loop-condition, remaining_fields == 0
+        //     dup 0
+        //     push 0
+        //     eq
+        //     skiz
+        //         return
 
+        //     // _ *discriminant [other_fields] *next_field next_size is_reference_type remaining_fields
 
-            // We cannot know how big `data` is, *and* we cannot know how many other
-            // fields there are. So the task is impossible. We need to access
-            // [(*data_field, stack_size)] from memory!
+        //     // We cannot know how big `data` is, *and* we cannot know how many other
+        //     // fields there are. So the task is impossible. We need to access
+        //     // [(*data_field, stack_size)] from memory!
 
-            // BUT: Let's just assume *one* field for now and get that to work
-            // TODO: This is wrong if there are multiple fields in the variant's
-            // data.
-            // _ *discriminant *next_field next_size remaining_fields
+        //     // BUT: Let's just assume *one* field for now and get that to work
+        //     // TODO: This is wrong if there are multiple fields in the variant's
+        //     // data.
+        //     // _ *discriminant *next_field next_size remaining_fields
 
-            pop
-            // _ *discriminant *next_field next_size is_reference_type
+        //     pop
+        //     // _ *discriminant *next_field next_size is_reference_type
 
-            swap 2
-            // _ *discriminant is_reference_type next_size *next_field
+        //     swap 2
+        //     // _ *discriminant is_reference_type next_size *next_field
 
-            dup 1
-            push -1
-            add
-            add
-            // _ *discriminant is_reference_type next_size *last_word_of_field
+        //     dup 1
+        //     push -1
+        //     add
+        //     add
+        //     // _ *discriminant is_reference_type next_size *last_word_of_field
 
-            swap 2
-            // _ *discriminant *last_word_of_field next_size is_reference_type
+        //     swap 2
+        //     // _ *discriminant *last_word_of_field next_size is_reference_type
 
-            dup 1
-            swap 1
-            // _ *discriminant *last_word_of_field next_size next_size is_reference_type
+        //     dup 1
+        //     swap 1
+        //     // _ *discriminant *last_word_of_field next_size next_size is_reference_type
 
-            call {load_field_data_from_memory_label}
-            // _ [field_data] *discriminant garbage data_size 0
+        //     call {load_field_data_from_memory_label}
+        //     // _ [field_data] *discriminant garbage data_size 0
 
-            pop
-            // _ [field_data] *discriminant garbage data_size
+        //     pop
+        //     // _ [field_data] *discriminant garbage data_size
 
-            swap 1
-            pop
-            // _ [field_data] *discriminant data_size
+        //     swap 1
+        //     pop
+        //     // _ [field_data] *discriminant data_size
 
-            // TODO: Change to `recurse` once above logic is fixed!
-            return
+        //     // TODO: Change to `recurse` once above logic is fixed!
+        //     return
+        // )
+        // .try_into()
+        // .unwrap();
+        // state.add_library_function(read_data_size_elements_to_stack_loop);
+
+        // let size_of_data_plus_padding = self.stack_size() - 1;
+        // code.append(&mut triton_asm!(
+        //     // _ *discriminant [(*data_field, stack_size)] remaining_fields
+        //     call {load_variant_data_from_memory_label}
+        //     // _ [field_data] *discriminant data_size
+
+        //     push -1
+        //     mul
+        //     // _ [field_data] *discriminant -data_size
+
+        //     push {size_of_data_plus_padding}
+        //     add
+        //     // _ [field_data] *discriminant padding_words
+        // ));
+
+        // let pad_subroutine_loop = pad_subroutine();
+        // let pad_subroutine_loop_label = pad_subroutine_loop.get_label();
+        // state.add_library_function(pad_subroutine_loop);
+        // code.append(&mut triton_asm!(
+        //     // _ [data] *discriminant padding_words
+        //     call { pad_subroutine_loop_label }
+        //     // _ [data] [padding'] *discriminant 0
+
+        //     pop
+        //     // _ [data] [padding'] *discriminant
+
+        //     read_mem
+        //     // _ [data] [padding'] *discriminant discriminant
+
+        //     swap 1
+        //     pop
+        //     // _ [data] [padding'] discriminant
+        // ));
+
+        // code
+    }
+
+    /// Return the code to get all field pointers for the discriminant whose
+    /// pointer lives on top of the stack.
+    /// The field pointers are stored in memory at a statically known address.
+    /// Returns (**fields, function to call, helper functions)
+    /// BEFORE: _ *discriminant
+    /// AFTER: _
+    fn get_variant_data_pointers(
+        &self,
+        state: &mut CompilerState,
+    ) -> (BFieldElement, SubRoutine, Vec<SubRoutine>) {
+        let max_field_count = self
+            .variants
+            .iter()
+            .map(|(_name, data_type)| data_type.as_tuple_type().element_count())
+            .max()
+            .unwrap_or_default();
+
+        // Allocate memory for all results, plus one word for length indication.
+        let pointer_for_result: u64 = state
+            .global_compiler_state
+            .snippet_state
+            .kmalloc(max_field_count + 1)
+            .try_into()
+            .unwrap();
+
+        assert!(
+            self.variants
+                .iter()
+                .all(|x| x.1.as_tuple_type().element_count() < 2),
+            "Cannot handle so much data in enums. enum type: \"{}\"",
+            self.name
+        );
+
+        let function_label = format!("get_variant_data_pointers_at_runtime_{}", self.name);
+
+        let mut subroutines: Vec<SubRoutine> = vec![];
+        let mut acc_code = triton_asm!(
+            {function_label}:
+        );
+
+        for (haystack_discriminant, (variant_name, variant_type)) in
+            self.variants.iter().enumerate()
+        {
+            let subroutine_label =
+                format!("{}_find_pointers_subroutine_{}", self.name, variant_name);
+            acc_code.append(&mut triton_asm!(
+                // *discriminant
+
+                read_mem
+                // *discriminant discriminant
+
+                push {haystack_discriminant}
+                eq
+                // *discriminant (discriminant == haystack)
+
+                skiz
+                call {subroutine_label}
+
+                // *discriminant
+            ));
+
+            let data_fields = variant_type.as_tuple_type();
+            let subroutine = match data_fields.element_count() {
+                0 => triton_asm!(
+                    {subroutine_label}:
+                        // _ *discriminant
+
+                        push {pointer_for_result}
+
+                        push 0
+                        // _ *discriminant **fields field_count
+
+                        write_mem
+                        // _ *discriminant **fields
+
+                        pop
+                        // _ *discriminant
+
+                        return
+                ),
+                1 => {
+                    let data_field = &data_fields[0];
+                    let static_encoding_length = data_field.bfield_codec_length();
+
+                    let get_first_field = match static_encoding_length {
+                        Some(_static_size) => triton_asm!(
+                            // _ *discriminant
+                            push 1
+                            add
+                            // _ *first_field
+                        ),
+                        None => triton_asm!(
+                            // _ *discriminant
+                            push 2
+                            add
+                            // _ *first_field
+                        ),
+                    };
+
+                    triton_asm!(
+                        {subroutine_label}:
+                        // _ *discriminant
+
+                        push {pointer_for_result}
+
+                        push 1
+                        // _ *discriminant **fields field_count
+
+                        write_mem
+                        // _ *discriminant **fields
+
+                        push 1
+                        add
+                        // _ *discriminant *fields[0]
+
+                        dup 1
+                        {&get_first_field}
+                        // _ *discriminant *fields[0] *first_field
+
+                        write_mem
+                        // _ *discriminant *fields[0]
+
+                        pop
+
+                        return
+                    )
+                }
+                _n => {
+                    todo!()
+                }
+            };
+
+            subroutines.push(subroutine.try_into().unwrap());
+        }
+
+        (
+            pointer_for_result.into(),
+            acc_code.try_into().unwrap(),
+            subroutines,
         )
-        .try_into()
-        .unwrap();
-        state.add_library_function(read_data_size_elements_to_stack_loop);
-
-        let size_of_data_plus_padding = self.stack_size() - 1;
-        code.append(&mut triton_asm!(
-            // _ *discriminant [(*data_field, stack_size)] remaining_fields
-            call {load_variant_data_from_memory_label}
-            // _ [field_data] *discriminant data_size
-
-            push -1
-            mul
-            // _ [field_data] *discriminant -data_size
-
-            push {size_of_data_plus_padding}
-            add
-            // _ [field_data] *discriminant padding_words
-        ));
-
-        let pad_subroutine_loop = pad_subroutine();
-        let pad_subroutine_loop_label = pad_subroutine_loop.get_label();
-        state.add_library_function(pad_subroutine_loop);
-        code.append(&mut triton_asm!(
-            // _ [data] *discriminant padding_words
-            call { pad_subroutine_loop_label }
-            // _ [data] [padding'] *discriminant 0
-
-            pop
-            // _ [data] [padding'] *discriminant
-
-            read_mem
-            // _ [data] [padding'] *discriminant discriminant
-
-            swap 1
-            pop
-            // _ [data] [padding'] discriminant
-        ));
-
-        code
     }
 
     /// Return the function to get data field pointers for variant data, at
     /// runtime.
     /// BEFORE: _ *discriminant
     /// AFTER: _ [(*data_field, stack_size, is_reference_type)] field_count
-    fn get_variant_data_pointers_and_field_sizes_at_runtime(
+    fn get_variant_data_pointers_and_field_sizes_at_runtime_old(
         &self,
     ) -> (SubRoutine, Vec<SubRoutine>) {
         // TODO: Return value could be delivered as pointer to
