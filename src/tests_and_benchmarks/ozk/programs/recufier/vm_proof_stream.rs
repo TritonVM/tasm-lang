bd43613a6f7f8b3f37236c88bd8431019278f982
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-01-30 12:04:13 +0100

    fix: correctly load log 2 padded height from proof
    Co-authored-by: sword-smith <thor@neptune.cash>

--- src/tests_and_benchmarks/ozk/programs/recufier/vm_proof_stream.rs
+++ src/tests_and_benchmarks/ozk/programs/recufier/vm_proof_stream.rs
@@ -1,3 +1,4 @@
+use num::One;
 use tasm_lib::triton_vm::prelude::*;
 
 use crate::tests_and_benchmarks::ozk::rust_shadows as tasm;
@@ -13,19 +14,25 @@ pub struct VmProofIter {
 impl VmProofIter {
     pub fn new() -> VmProofIter {
         return VmProofIter {
-            current_item_pointer: 1,
+            current_item_pointer: 2,
         };
     }
 
     /// # Panics
     /// Panics if there is no next item.
-    pub fn next_as_log_2_padded_height(&self) -> u32 {
+    pub fn next_as_log_2_padded_height(&mut self) -> u32 {
         let current_item_size_pointer: BFieldElement =
             BFieldElement::new(self.current_item_pointer as u64);
-        let proof_item: Box<ProofItem> =
-            ProofItem::decode(&tasm::load_from_memory(current_item_size_pointer)).unwrap();
+        let current_item_pointer: BFieldElement = current_item_size_pointer + BFieldElement::one();
 
-        return match proof_item.as_ref() {
+        // Super specific knowledge about the encoding of `ProofItem::Log2PaddedHeight`.
+        self.current_item_pointer += 3;
+
+        let log_2_padded_height_item: Box<ProofItem> =
+            ProofItem::decode(&tasm::load_from_memory(current_item_pointer)).unwrap();
+        bfield_codec::decode_from_memory::<ProofItem>(current_item_pointer).unwrap();
+
+        return match log_2_padded_height_item.as_ref() {
             ProofItem::Log2PaddedHeight(height) => {
                 //
                 *height
