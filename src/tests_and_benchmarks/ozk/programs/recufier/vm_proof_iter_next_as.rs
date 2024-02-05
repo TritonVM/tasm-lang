ecc0d4e4542228bf7d1eb3cd677c6759eceeb072
sword_smith <thor@neptune.cash>
2024-02-05 23:03:19 +0100

    Fix `next_as_` test for VmProofIter internal struct
    The `next_as_` snippets in `tasm-lib` that the `next_as` methods on `VmProofIter`
    are thin wrappers around returns either a pointer to a payload size, or a pointer
    to the payload. With this change, the wrapper-methods always return a pointer to
    the payload.

--- src/tests_and_benchmarks/ozk/programs/recufier/vm_proof_iter_next_as.rs
+++ src/tests_and_benchmarks/ozk/programs/recufier/vm_proof_iter_next_as.rs
@@ -1,5 +1,5 @@
 use tasm_lib::triton_vm::prelude::*;
-use tasm_lib::triton_vm::proof_item::ProofItem;
+use tasm_lib::triton_vm::proof_item::{FriResponse, ProofItem};
 use tasm_lib::triton_vm::proof_stream::ProofStream;
 use tasm_lib::twenty_first::shared_math::tip5::Tip5State;
 use tasm_lib::twenty_first::util_types::algebraic_hasher::SpongeHasher;
@@ -20,6 +20,94 @@ fn call_all_next_methods() {
         vm_proof_iter.next_as_outofdomainbaserow(&mut sponge_state);
     tasm::tasm_io_write_to_stdout___xfe(out_of_domain_base_row[0]);
     tasm::tasm_io_write_to_stdout___xfe(out_of_domain_base_row[1]);
+
+    let out_of_domain_ext_row: Box<Vec<XFieldElement>> =
+        vm_proof_iter.next_as_outofdomainextrow(&mut sponge_state);
+    tasm::tasm_io_write_to_stdout___xfe(out_of_domain_ext_row[0]);
+    tasm::tasm_io_write_to_stdout___xfe(out_of_domain_ext_row[1]);
+
+    let out_of_domain_quotient_segments: Box<[XFieldElement; 4]> =
+        vm_proof_iter.next_as_outofdomainquotientsegments(&mut sponge_state);
+    tasm::tasm_io_write_to_stdout___xfe(out_of_domain_quotient_segments[0]);
+    tasm::tasm_io_write_to_stdout___xfe(out_of_domain_quotient_segments[1]);
+    tasm::tasm_io_write_to_stdout___xfe(out_of_domain_quotient_segments[2]);
+    tasm::tasm_io_write_to_stdout___xfe(out_of_domain_quotient_segments[3]);
+
+    let authentication_structure: Box<Vec<Digest>> =
+        vm_proof_iter.next_as_authenticationstructure(&mut sponge_state);
+    tasm::tasm_io_write_to_stdout___digest(authentication_structure[0]);
+    tasm::tasm_io_write_to_stdout___digest(authentication_structure[1]);
+    tasm::tasm_io_write_to_stdout___digest(authentication_structure[2]);
+
+    let mbtw: Box<Vec<Vec<BFieldElement>>> =
+        vm_proof_iter.next_as_masterbasetablerows(&mut sponge_state);
+    {
+        let mut j: usize = 0;
+        while j < mbtw.len() {
+            let mut i: usize = 0;
+            while i < mbtw[j].len() {
+                tasm::tasm_io_write_to_stdout___bfe(mbtw[j][i]);
+                i += 1;
+            }
+            j += 1;
+        }
+    }
+
+    let metr: Box<Vec<Vec<XFieldElement>>> =
+        vm_proof_iter.next_as_masterexttablerows(&mut sponge_state);
+    {
+        let mut j: usize = 0;
+        while j < metr.len() {
+            let mut i: usize = 0;
+            while i < metr[j].len() {
+                tasm::tasm_io_write_to_stdout___xfe(metr[j][i]);
+                i += 1;
+            }
+            j += 1;
+        }
+    }
+
+    let log2paddedheight: Box<u32> = vm_proof_iter.next_as_log2paddedheight(&mut sponge_state);
+    tasm::tasm_io_write_to_stdout___u32(*log2paddedheight);
+
+    let quotient_segments_elements: Box<Vec<[XFieldElement; 4]>> =
+        vm_proof_iter.next_as_quotientsegmentselements(&mut sponge_state);
+    {
+        let mut j: usize = 0;
+        while j < quotient_segments_elements.len() {
+            tasm::tasm_io_write_to_stdout___xfe(quotient_segments_elements[j][0]);
+            tasm::tasm_io_write_to_stdout___xfe(quotient_segments_elements[j][1]);
+            tasm::tasm_io_write_to_stdout___xfe(quotient_segments_elements[j][2]);
+            tasm::tasm_io_write_to_stdout___xfe(quotient_segments_elements[j][3]);
+            j += 1;
+        }
+    }
+
+    let fri_codeword: Box<Vec<XFieldElement>> =
+        vm_proof_iter.next_as_fricodeword(&mut sponge_state);
+    {
+        let mut j: usize = 0;
+        while j < fri_codeword.len() {
+            tasm::tasm_io_write_to_stdout___xfe(fri_codeword[j]);
+            j += 1;
+        }
+    }
+
+    let fri_response: Box<FriResponse> = vm_proof_iter.next_as_friresponse(&mut sponge_state);
+    {
+        let mut j: usize = 0;
+        while j < fri_response.auth_structure.len() {
+            tasm::tasm_io_write_to_stdout___digest(fri_response.auth_structure[j]);
+            j += 1;
+        }
+    }
+    {
+        let mut j: usize = 0;
+        while j < fri_response.revealed_leaves.len() {
+            tasm::tasm_io_write_to_stdout___xfe(fri_response.revealed_leaves[j]);
+            j += 1;
+        }
+    }
 
     return;
 }
@@ -96,7 +184,7 @@ mod test {
     }
 
     fn arbitrary_out_of_domain_ext_row() -> Vec<XFieldElement> {
-        (600u64..642)
+        (600u64..603)
             .map(|i| XFieldElement::new([i.into(), (i + 2).into(), (i + 5).into()]))
             .collect_vec()
     }
@@ -110,7 +198,7 @@ mod test {
     }
 
     fn arbitrary_auth_structure() -> Vec<Digest> {
-        (400u64..420)
+        (400u64..403)
             .map(|i| {
                 Digest::new([
                     i.into(),
@@ -205,27 +293,27 @@ mod test {
         proof_stream.enqueue(ProofItem::OutOfDomainBaseRow(
             arbitrary_out_of_domain_base_row(),
         ));
-        // proof_stream.enqueue(ProofItem::OutOfDomainExtRow(
-        //     arbitrary_out_of_domain_ext_row(),
-        // ));
-        // proof_stream.enqueue(ProofItem::OutOfDomainQuotientSegments(
-        //     arbitrary_out_of_domain_quotient_segments(),
-        // ));
-        // proof_stream.enqueue(ProofItem::AuthenticationStructure(
-        //     arbitrary_auth_structure(),
-        // ));
-        // proof_stream.enqueue(ProofItem::MasterBaseTableRows(
-        //     arbitrary_master_base_table_rows(),
-        // ));
-        // proof_stream.enqueue(ProofItem::MasterExtTableRows(
-        //     arbitrary_ext_base_table_rows(),
-        // ));
-        // proof_stream.enqueue(ProofItem::Log2PaddedHeight(arbitrary_log2_padded_height()));
-        // proof_stream.enqueue(ProofItem::QuotientSegmentsElements(
-        //     arbitrary_quotient_segments_elements(),
-        // ));
-        // proof_stream.enqueue(ProofItem::FriCodeword(arbitrary_fri_codeword()));
-        // proof_stream.enqueue(ProofItem::FriResponse(arbitrary_fri_response()));
+        proof_stream.enqueue(ProofItem::OutOfDomainExtRow(
+            arbitrary_out_of_domain_ext_row(),
+        ));
+        proof_stream.enqueue(ProofItem::OutOfDomainQuotientSegments(
+            arbitrary_out_of_domain_quotient_segments(),
+        ));
+        proof_stream.enqueue(ProofItem::AuthenticationStructure(
+            arbitrary_auth_structure(),
+        ));
+        proof_stream.enqueue(ProofItem::MasterBaseTableRows(
+            arbitrary_master_base_table_rows(),
+        ));
+        proof_stream.enqueue(ProofItem::MasterExtTableRows(
+            arbitrary_ext_base_table_rows(),
+        ));
+        proof_stream.enqueue(ProofItem::Log2PaddedHeight(arbitrary_log2_padded_height()));
+        proof_stream.enqueue(ProofItem::QuotientSegmentsElements(
+            arbitrary_quotient_segments_elements(),
+        ));
+        proof_stream.enqueue(ProofItem::FriCodeword(arbitrary_fri_codeword()));
+        proof_stream.enqueue(ProofItem::FriResponse(arbitrary_fri_response()));
         proof_stream.into()
     }
 }
